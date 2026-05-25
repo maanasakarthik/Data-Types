6643ceaca1781e3729bb5e1720567afb10d9eb0e
maanasakarthik <maanasakarthik1@gmail.com>
2026-05-25 18:34:01 +0530

    Typecasting

---
+++ main2.py
@@ -0,0 +1,14 @@
+# Typecasting
+age = 17
+weight = 49.2
+
+#before type casting
+print("Data Type of age is", type(age))
+print("Data Type of weight is", type(weight))
+
+#type casting conversion
+print("\n After Type Casting ...")
+age = str(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(" Data Type of weight is", type(weight))
