1155131d5da731988d0321cbe586347e72a0a431
maanasakarthik <maanasakarthik1@gmail.com>
2026-05-25 18:42:49 +0530

    Reverse String Operation

---
+++ main3.py
@@ -0,0 +1,8 @@
+#input a word
+text = str(input("Enter a string: "))
+
+#Reverse String
+# using step value -1 to iterate in reverse
+revText = text[::-1]
+
+print("Reverse of Given String is : " , revText)
