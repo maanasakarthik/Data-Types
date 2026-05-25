9e72110626c38c5b876b19a8c3421ac23cdd7793
maanasakarthik <maanasakarthik1@gmail.com>
2026-05-25 18:21:54 +0530

    Checking the datatypes of different values

---
+++ main.py
@@ -0,0 +1,12 @@
+#Check the datatype of different values
+a = 5
+print("type of a: ", type(a))
+
+b = 3.1415
+print("type of b: ", type(b))
+
+c = "Codingal"
+print("type of c: ", type(c))
+
+d = False
+print("type of d: ", type(d))
